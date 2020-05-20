cf1c7536e9573f2322bf8d055932f8ab62750487
John Wo <labmansoftac@gmail.com>
2020-05-20 13:12:35 +0300

    Store modules added

--- 3.Firebase_Viewjs/chat/src/guards/auth.guard.js
+++ 3.Firebase_Viewjs/chat/src/guards/auth.guard.js
@@ -1,8 +1,8 @@
-import router from '../router';
+import router from '@/router';
 
 export default function authGuard(store) {
   const isAuth = store.state.auth.isLoggedIn;
-  const authRoutes = ['Login'];
+  const authRoutes = ['Login', 'SignUp'];
 
   router.beforeEach((to, from, next) => {
     console.log(to);
--- 3.Firebase_Viewjs/chat/src/router/index.js
+++ 3.Firebase_Viewjs/chat/src/router/index.js
@@ -18,6 +18,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue'),
   },
+  {
+    path: '/signup',
+    name: 'SignUp',
+    component: () => import(/* webpackChunkName: "signup */ '../views/SignUp.vue'),
+  },
 ];
 
 const router = new VueRouter({
