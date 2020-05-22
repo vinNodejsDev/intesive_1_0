f78964b90484dddb7c8e0772fffacc2072417d36
John Wo <labmansoftac@gmail.com>
2020-05-22 13:03:01 +0300

    auth.guard edited

--- 3.Firebase_Viewjs/chat/src/guards/auth.guard.js
+++ 3.Firebase_Viewjs/chat/src/guards/auth.guard.js
@@ -1,17 +1,16 @@
 import router from '@/router';
 
 export default function authGuard(store) {
-  const isAuth = store.state.auth.isLoggedIn;
   const authRoutes = ['Login', 'SignUp'];
 
   router.beforeEach((to, from, next) => {
     console.log(to);
 
-    if (authRoutes.includes(to.name) && isAuth) {
+    if (authRoutes.includes(to.name) && store.state.auth.isLoggedIn) {
       return next({ name: 'Home' });
     }
 
-    if (!authRoutes.includes(to.name) && !isAuth) {
+    if (!authRoutes.includes(to.name) && !store.state.auth.isLoggedInuth) {
       return next({ name: 'Login' });
     }
 
