44516466f80d69544affa5a01a2771df52cd0e4f
John Wo <labmansoftac@gmail.com>
2020-05-22 13:04:02 +0300

    axios configs changed

--- 3.Firebase_Viewjs/chat/src/plugins/axios/index.js
+++ 3.Firebase_Viewjs/chat/src/plugins/axios/index.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 import initInterceptors from './interceptors';
 
 const instance = axios.create({
-  baseURL: ProcessingInstruction.env.VUE_APP_URL,
+  baseURL: process.env.VUE_APP_URL,
 });
 
 initInterceptors(instance);
+
 export default instance;
