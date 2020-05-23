4885c8c7f36cf2f842561168dead22cf0704cb4d
John Wo <labmansoftac@gmail.com>
2020-05-23 13:08:16 +0300

    User model added

---
+++ 4.Backend_Development/server/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  country: {
+    type: String,
+    required: false
+  },
+  city: {
+    type: String 
+  }
+});
+
+mongoose.model('User', UserSchema);
+
+module.exports = mongoose.model('User');
