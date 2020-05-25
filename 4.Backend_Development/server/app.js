700a80efa6375adab1ec618a48772b411d1d4438
John Wo <labmansoftac@gmail.com>
2020-05-25 13:11:46 +0300

    Modified Backend

--- 4.Backend_Development/server/app.js
+++ 4.Backend_Development/server/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { url } = require('./config/db');
 const app = express();
 const UserController = require('./routes/user');
 
@@ -10,8 +12,16 @@ const PORT = 3000;
 
 app.use(express.json());
 
+//connect to DB
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('Mongo connected'))
+  .catch(err => console.log(err));
+
 app.use(ROUTES.users, UserController);
 
 app.listen(PORT, () => {
   console.log(`Server up and is running on port ${PORT}`)
-});
+}); 
